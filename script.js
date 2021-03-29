04a85ea722af043ef9f9fdf943860c0412615703
jcgthomas <79747978+jcgthomas@users.noreply.github.com>
2021-03-29 15:09:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 $("document").ready(() => {
 
-    $(".introText").hover(function() {
+    $(".introText").hover(() => {
         $("#james").css( "color", "var(--col5)" );
-      }, function() {
+      }, () => {
         $("#james").css( "color", "rgba(255, 255, 255, 0.8)" );
       })
-
 });
 
-
-let cvHighlight = function() {
-  $("#cv").toggleClass("cvHighlight");
-
-}
