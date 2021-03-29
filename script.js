2722ed48f0970a407242a69a8fd232573adad841
jcgthomas <79747978+jcgthomas@users.noreply.github.com>
2021-03-29 17:05:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ $("document").ready(() => {
       })
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
