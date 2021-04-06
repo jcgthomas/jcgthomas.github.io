7fc8fd18b4e634e8ddf35ea5fc98a0c98235ca4f
jcgthomas <79747978+jcgthomas@users.noreply.github.com>
2021-04-06 16:18:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,25 +21,26 @@ $("document").ready(() => {
             let n = Math.floor(Math.random()*100);
             let o = Math.floor(Math.random()*100);
             let p = Math.floor(Math.random()*100);
-            let q = Math.floor(Math.random()*100);
-            $(j).css("opacity", `${(l/10)+1}%`)
-                .css("transition", `${(k/10)+1}s`)
+            $(j).css("opacity", `${(l-50)/1.2}%`)
+                .css("transition", `${(k/10)+2}s`)
                 .css("background", `hsl(0, 0%, ${l}%)`)
                 .css("transform", `translate(${(m/100)-0.5}vw, ${(n/100)-0.5}vh)
                                    rotate(${(o/20)-2.5}deg)
-                                   scale(${(p/500)+0.9}, ${(p/500)+0.9})`);
+                                   scale(${(p/500)+0.9}, ${(p/500)+0.9})`)
+                .css("box-shadow", `0 0 20px hsl(0, 0%, ${l}%)`);
         }
 
         function hoverSquare() {
             let r = Math.floor(Math.random()*359);
-            $(j).css("opacity", "40%")
+            $(j).css("opacity", "80%")
                 .css("transition", "0.15s")
-                .css("background", `hsl(${r}, 80%, 60%)`)
+                .css("background", `hsl(${r}, 30%, 50%)`)
+                .css("box-shadow", `0 0 50px hsl(${r}, 30%, 50%)`);
                 // .css("transform", `scale(1.05, 1.05) rotate(${(r/20)-9}deg)`);
                 // .css("transform", "scale(1,1)");
         }
         animateSquare();
-        setInterval(animateSquare, Math.floor(Math.random()*100)*100+8000);
+        setInterval(animateSquare, Math.floor(Math.random()*100)*300+10000);
         $(j).hover(hoverSquare, animateSquare);
     })
 });
